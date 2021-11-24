ea267e9000f5e06d3f1755cf7201d46ba4ec58ee
root <lawrencemsteven@gmail.com>
2021-11-24 21:06:48 +0000

    Error Handling

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from pages.mood import mood
 from flask_cors import CORS
 from models import db, User, guard
 from flask_praetorian import auth_required, current_user
+from flask_praetorian.exceptions import PraetorianError as PraetorianError
 
 app = Flask(__name__, static_folder="build/static", template_folder="build")
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SECRET_KEY'] = '\xd2\xe9sxkk\xb4\xd1\xd3$\xd2U\xf8F)\xd5W\x8eo\xb4\x18\x864J'
+app.config['SECRET_KEY'] = 'X\x9f\xb2\x92\n\xec\x91r\xcd?\xae\x9a%\xe4\xe3\xa0\x7f[\xed\xa9\x92\x8d\x0b\x17'
 app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
 app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
 
@@ -24,6 +25,16 @@ with app.app_context():
 	if db.session.query(User).filter_by(username='admin').count() < 1:
 		db.session.add(User(username='admin', password=guard.hash_password('admin'), roles='admin'))
 	db.session.commit()
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+	print("Error")
+	return index()
+
+@app.errorhandler(PraetorianError)
+def praetorian_error(e):
+	print("Praetorian Error")
+	return index()
 
 @app.route("/")
 def index():
